71e66749b9a372dced68dbb7cb09da6ada03419a
nagiaboushaca <nagishaca@gmail.com>
2023-12-06 11:19:17 -0500

    a6 done

--- app.js
+++ app.js
@@ -3,13 +3,29 @@ import Hello from "./hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from './courses/routes.js';
 import ModuleRoutes from './modules/routes.js';
+import UserRoutes from './users/routes.js';
 import cors from "cors";
+import mongoose from "mongoose";
+import session from 'express-session';
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 import "dotenv/config";
 
 // const express = require('express')
 const app = express()
-app.use(cors());
+app.use(cors({   credentials: true,
+        origin: "http://localhost:3000",
+    }));
+// const sessionOptions = {
+//     secret: "any string",
+//     resave: false,
+//     saveUninitialized: false,
+// };
+// app.use(
+//     session(sessionOptions)
+// );
+      
 app.use(express.json());
+UserRoutes(app)
 CourseRoutes(app)
 ModuleRoutes(app)
 Lab5(app)
